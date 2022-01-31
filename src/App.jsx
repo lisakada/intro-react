61bff31a8f2f975d45f34696e1910c8578f97f9c
Elizabeth Michael <lisakada@gmail.com>
2022-01-31 08:49:21 -0600

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -58,7 +58,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
@@ -89,7 +89,7 @@ class App extends React.Component {
       <div>
         <div className="Box Box--spacious f4">
           <div className="Box-header">
-          <h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>
+            <h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>
           </div>
         </div>
         <nav className="UnderlineNav d-flex flex-justify-center">
